40a9a4b2cc34fe7ce6a01face198015e58733939
Shad Storhaug <shad@shadstorhaug.com>
2014-12-03 23:11:49 +0700

    Added DocIdSetUtil to the BoboBrowse.Net.Util namespace.

---
+++ src/BoboBrowse.Net/Util/DocIdSetUtil.cs
@@ -0,0 +1,30 @@
+﻿namespace BoboBrowse.Net.Util
+{
+    using Lucene.Net.Search;
+    using System.Text;
+
+    public static class DocIdSetUtil
+    {
+        public static string AsString(this DocIdSet docIdSet)
+        {
+            DocIdSetIterator iter = docIdSet.Iterator();
+            StringBuilder buf = new StringBuilder();
+            bool firstTime = true;
+            buf.Append("[");
+            while (iter.NextDoc() != DocIdSetIterator.NO_MORE_DOCS)
+            {
+                if (firstTime)
+                {
+                    firstTime = false;
+                }
+                else
+                {
+                    buf.Append(",");
+                }
+                buf.Append(iter.DocID());
+            }
+            buf.Append("]");
+            return buf.ToString();
+        }
+    }
+}
