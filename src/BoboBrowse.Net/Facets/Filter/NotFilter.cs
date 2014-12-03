13203c591fcb752928fe21e5940fdcda41673a37
Shad Storhaug <shad@shadstorhaug.com>
2014-12-03 19:54:20 +0700

    Added the NotFilter and OrFilter to the BoboBrowse.Net.Facets.Filter namespace.

---
+++ src/BoboBrowse.Net/Facets/Filter/NotFilter.cs
@@ -0,0 +1,21 @@
+namespace BoboBrowse.Net.Facets.Filter
+{
+    using Lucene.Net.Index;
+    using Lucene.Net.Search;
+    using LuceneExt;
+
+    public class NotFilter : Filter
+    {
+        private readonly Filter _innerFilter;
+
+        public NotFilter(Filter innerFilter)
+        {
+            _innerFilter = innerFilter;
+        }
+
+        public override DocIdSet GetDocIdSet(IndexReader reader)
+        {
+            return new NotDocIdSet(_innerFilter.GetDocIdSet(reader), reader.MaxDoc);
+        }
+    }
+}
---
+++ src/BoboBrowse.Net/Facets/Filter/OrFilter.cs
@@ -0,0 +1,34 @@
+namespace BoboBrowse.Net.Facets.Filter
+{
+    using System.Collections.Generic;
+    using Lucene.Net.Index;
+    using Lucene.Net.Search;
+    using LuceneExt;
+
+    public class OrFilter : Filter
+    {
+        private readonly List<Filter> _filters;
+
+        public OrFilter(List<Filter> filters)
+        {
+            _filters = filters;
+        }
+
+        public override DocIdSet GetDocIdSet(IndexReader reader)
+        {
+            if (_filters.Count == 1)
+            {
+                return _filters[0].GetDocIdSet(reader);
+            }
+            else
+            {
+                List<DocIdSet> list = new List<DocIdSet>(_filters.Count);
+                foreach (Filter f in _filters)
+                {
+                    list.Add(f.GetDocIdSet(reader));
+                }
+                return new OrDocIdSet(list);
+            }
+        }
+    }
+}
