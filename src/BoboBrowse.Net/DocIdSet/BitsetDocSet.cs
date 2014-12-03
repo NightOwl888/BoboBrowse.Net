7d5381f38819f1b1f40c04baa418786192996fd5
Shad Storhaug <shad@shadstorhaug.com>
2014-12-03 15:50:54 +0700

    Added missing BitsetDocSet implementation.

---
+++ src/BoboBrowse.Net/DocIdSet/BitsetDocSet.cs
@@ -0,0 +1,75 @@
+﻿
+
+namespace BoboBrowse.Net.DocIdSet
+{
+    using BoboBrowse.Net.Util;
+    using Lucene.Net.Search;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BitsetDocSet : DocIdSet
+    {
+        private readonly BitSet _bs;
+
+        public BitsetDocSet()
+        {
+            _bs = new BitSet();
+        }
+
+        public BitsetDocSet(int nbits)
+        {
+            _bs = new BitSet(nbits);
+        }
+
+        public int Size
+        {
+            get { return _bs.Cardinality(); }
+        }
+
+        public override DocIdSetIterator Iterator()
+        {
+            return new BitsDocIdSetIterator(_bs);
+        }
+
+        public class BitsDocIdSetIterator : DocIdSetIterator
+        {
+            private readonly BitSet _bs;
+            private int _current;
+
+            public BitsDocIdSetIterator(BitSet bs)
+            {
+                _bs = bs;
+                _current = -1;
+            }
+
+            public override int DocID()
+            {
+                return _current;
+            }
+
+            public override int NextDoc()
+            {
+                _current = _bs.NextSetBit(_current + 1);
+                if (_current != -1)
+                {
+                    return _current;
+                }
+
+                return DocIdSetIterator.NO_MORE_DOCS;
+            }
+
+            public override int Advance(int target)
+            {
+                _current = _bs.NextSetBit(target);
+                if (_current != -1)
+                {
+                    return _current;
+                }
+
+                return DocIdSetIterator.NO_MORE_DOCS;
+            }
+        }
+    }
+}
