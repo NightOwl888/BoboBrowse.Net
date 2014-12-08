17b986038cca0d972433cf568738ac9c9d0d2608
Shad Storhaug <shad@shadstorhaug.com>
2014-12-08 16:29:32 +0700

    Updated classes in the Facets namespace. Deleted IFacetHandlerFactory because it is not in BoboBrowse 3.1.0.

--- src/BoboBrowse.Net/Facets/IFacetCountCollector.cs
+++ src/BoboBrowse.Net/Facets/IFacetCountCollector.cs
@@ -1,25 +1,27 @@
-﻿
-
+﻿// Version compatibility level: 3.1.0
 namespace BoboBrowse.Net.Facets
 {
     using System;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Collects facet counts for a given browse request
+    /// </summary>
     public interface IFacetCountCollector : IFacetAccessible
     {
-        ///<summary>Collect a hit. This is called for every hit, thus the implementation needs to be super-optimized. </summary>
+        ///<summary>Collect a hit. This is called for every hit, thus the implementation needs to be super-optimized.</summary>
         ///<param name="docid"> doc </param>
         void Collect(int docid);
 
-        ///<summary>Collects all hits. This is called once per request by the facet engine in certain scenarios.  </summary>
+        ///<summary>Collects all hits. This is called once per request by the facet engine in certain scenarios.</summary>
         void CollectAll();
 
         ///<summary>Gets the name of the facet </summary>
         ///<returns>facet name </returns>
         string Name { get; }
 
-        ///<summary>Returns an integer array representing the distribution function of a given facet. </summary>
-        ///<returns> integer array of count values representing distribution of the facet values. </returns>
+        ///<summary>Returns an integer array representing the distribution function of a given facet.</summary>
+        ///<returns>integer array of count values representing distribution of the facet values.</returns>
         int[] GetCountDistribution();
     }
 
