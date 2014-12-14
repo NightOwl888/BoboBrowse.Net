052f43225fff9749fa58398e72630a09bb8c8743
Shad Storhaug <shad@shadstorhaug.com>
2014-12-14 22:30:30 +0700

    Upgraded ListMerger to version 3.1.0 compatibility.

--- src/BoboBrowse.Net/Util/ListMerger.cs
+++ src/BoboBrowse.Net/Util/ListMerger.cs
@@ -21,22 +21,26 @@
 //* please go to https://sourceforge.net/projects/bobo-browse/, or 
 //* send mail to owner@browseengine.com. 
 
+﻿// Version compatibility level: 3.1.0
+// EXCEPTION: We are using object instead of string for the BrowseFacet.Value property.
 namespace BoboBrowse.Net.Util
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Lucene.Net.Util;
     using BoboBrowse.Net.Facets.Impl;
     using BoboBrowse.Net.Search;
+    using Lucene.Net.Util;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
 
     public class ListMerger
     {
-        public class MergedIterator<T>
+        public class MergedIterator<T> : IEnumerator<T>
         {
             private class IteratorNode
             {
-                private readonly IEnumerator<T> iterator;
+                public IEnumerator<T> iterator;
                 public T CurVal;
 
                 public IteratorNode(IEnumerator<T> iterator)
@@ -73,27 +77,28 @@ namespace BoboBrowse.Net.Util
                 }
             }
 
-            private readonly MergedQueue queue;
+            private readonly MergedQueue _queue;
+            private T _current;
 
             private MergedIterator(int length, IComparer<T> comparator)
             {
-                queue = new MergedQueue(length, comparator);
+                _queue = new MergedQueue(length, comparator);
             }
 
-            public MergedIterator(ICollection<IEnumerable<T>> iterators, IComparer<T> comparator)
-                : this(iterators.Count, comparator)
+            public MergedIterator(IEnumerable<IEnumerator<T>> iterators, IComparer<T> comparator)
+                : this(iterators.Count(), comparator)
             {
                 foreach (IEnumerator<T> iterator in iterators)
                 {
                     IteratorNode ctx = new IteratorNode(iterator);
                     if (ctx.Fetch())
                     {
-                        queue.InsertWithOverflow(ctx);
+                        _queue.Add(ctx); // NOTE: This was InsertWithOverflow in codeplex version
                     }
                 }
             }
 
-            public MergedIterator(IEnumerable<T>[] iterators, IComparer<T> comparator)
+            public MergedIterator(IEnumerator<T>[] iterators, IComparer<T> comparator)
                 : this(iterators.Length, comparator)
             {
                 foreach (IEnumerator<T> iterator in iterators)
@@ -101,32 +106,74 @@ namespace BoboBrowse.Net.Util
                     IteratorNode ctx = new IteratorNode(iterator);
                     if (ctx.Fetch())
                     {
-                        queue.InsertWithOverflow(ctx);
+                        _queue.Add(ctx); // NOTE: This was InsertWithOverflow in codeplex version
                     }
                 }
             }
 
-            public virtual bool HasNext()
+            //public virtual bool HasNext()
+            //{
+            //    return queue.Size() > 0;
+            //}
+
+            //public virtual T Next()
+            //{
+            //    IteratorNode ctx = (IteratorNode)queue.Top();
+            //    T val = ctx.CurVal;
+            //    if (ctx.Fetch())
+            //    {
+            //        queue.UpdateTop();
+            //    }
+            //    else
+            //    {
+            //        queue.Pop();
+            //    }
+            //    return val;
+            //}
+
+            //public virtual void Remove()
+            //{
+            //    throw new NotSupportedException();
+            //}
+
+            public T Current
             {
-                return queue.Size() > 0;
+                get { return _current; }
             }
 
-            public virtual T Next()
+            public void Dispose()
             {
-                IteratorNode ctx = (IteratorNode)queue.Top();
-                T val = ctx.CurVal;
-                if (ctx.Fetch())
+            }
+
+            object IEnumerator.Current
+            {
+                get { return _current; }
+            }
+
+            public bool MoveNext()
+            {
+                if (_queue.Size() > 0)
                 {
-                    queue.UpdateTop();
+                    IteratorNode ctx = (IteratorNode)_queue.Top();
+                    T val = ctx.CurVal;
+                    if (ctx.Fetch())
+                    {
+                        _queue.UpdateTop();
+                    }
+                    else
+                    {
+                        _queue.Pop();
+                    }
+                    this._current = val;
+                    return true;
                 }
                 else
                 {
-                    queue.Pop();
+                    return false;
                 }
-                return val;
             }
 
-            public virtual void Remove()
+            public void Reset()
             {
                 throw new NotSupportedException();
             }
@@ -136,129 +183,148 @@ namespace BoboBrowse.Net.Util
         {
         }
 
-        public static MergedIterator<T> MergeLists<T>(IEnumerable<T>[] iterators, IComparer<T> comparator)
+        public static MergedIterator<T> MergeLists<T>(IEnumerator<T>[] iterators, IComparer<T> comparator)
         {
             return new MergedIterator<T>(iterators, comparator);
         }
 
-        public static MergedIterator<T> MergeLists<T>(ICollection<IEnumerable<T>> iterators, IComparer<T> comparator)
+        public static MergedIterator<T> MergeLists<T>(IEnumerable<IEnumerator<T>> iterators, IComparer<T> comparator)
         {
             return new MergedIterator<T>(iterators, comparator);
         }
 
-        public static List<T> MergeLists<T>(int offset, int count, IEnumerable<T>[] iterators, IComparer<T> comparator)
+        public static List<T> MergeLists<T>(int offset, int count, IEnumerator<T>[] iterators, IComparer<T> comparator)
         {
             return MergeLists(offset, count, new MergedIterator<T>(iterators, comparator));
         }
 
-        public static List<T> MergeLists<T>(int offset, int count, ICollection<IEnumerable<T>> iterators, IComparer<T> comparator)
+        public static List<T> MergeLists<T>(int offset, int count, IEnumerable<IEnumerator<T>> iterators, IComparer<T> comparator)
         {
             return MergeLists(offset, count, new MergedIterator<T>(iterators, comparator));
         }
 
         private static List<T> MergeLists<T>(int offset, int count, MergedIterator<T> mergedIter)
         {
-            for (int c = 0; c < offset && mergedIter.HasNext(); c++)
+            for (int c = 0; c < offset && mergedIter.MoveNext(); c++)
             {
-                var x = mergedIter.Next();
+                var x = mergedIter.Current;
             }
 
             List<T> mergedList = new List<T>();
 
-            for (int c = 0; c < count && mergedIter.HasNext(); c++)
+            for (int c = 0; c < count && mergedIter.MoveNext(); c++)
             {
-                mergedList.Add(mergedIter.Next());
+                mergedList.Add(mergedIter.Current);
             }
 
             return mergedList;
         }
 
-        public static IComparer<BrowseFacet> FACET_VAL_COMPARATOR = new MultiBoboBrowser.BrowseFacetValueComparator();
+        public static IComparer<BrowseFacet> FACET_VAL_COMPARATOR = new FacetValComparator();
 
-        public static Dictionary<string, IFacetAccessible> MergeSimpleFacetContainers(ICollection<Dictionary<string, IFacetAccessible>> subMaps, BrowseRequest req)
+        private class FacetValComparator : IComparer<BrowseFacet>
         {
-            Dictionary<string, Dictionary<object, BrowseFacet>> counts = new Dictionary<string, Dictionary<object, BrowseFacet>>();
-            foreach (Dictionary<string, IFacetAccessible> subMap in subMaps)
+            // FIXME: we need to reorganize all that stuff with comparators
+            private IComparer valueComparer = new Comparer(CultureInfo.InvariantCulture);
+
+            public int Compare(BrowseFacet o1, BrowseFacet o2)
             {
-                foreach (KeyValuePair<string, IFacetAccessible> entry in subMap)
-                {
-                    Dictionary<object, BrowseFacet> count = counts[entry.Key];
-                    if (count == null)
-                    {
-                        count = new Dictionary<object, BrowseFacet>();
-                        counts.Add(entry.Key, count);
-                    }
-                    foreach (BrowseFacet facet in entry.Value.GetFacets())
-                    {
-                        object val = facet.Value;
-                        BrowseFacet oldValue = count[val];
-                        if (oldValue == null)
-                        {
-                            count.Add(val, new BrowseFacet(val, facet.HitCount));
-                        }
-                        else
-                        {
-                            oldValue.HitCount = oldValue.HitCount + facet.HitCount;
-                        }
-                    }
-                }
+                return valueComparer.Compare(o1.Value, o2.Value);
+
+                //// NOTE: In this version we don't have to do it this way because the
+                //// values are stored in type object in the BrowseFacet.
+                //int ret = o1.Value.CompareTo(o2.Value);
+                //if (o1.getValue().startsWith("-") && o2.getValue().startsWith("-")) {
+                //    ret =-ret;
+                //}
+                //return ret;
             }
 
-            Dictionary<string, IFacetAccessible> mergedFacetMap = new Dictionary<string, IFacetAccessible>();
-
-            foreach (string facet in counts.Keys)
+            public static IDictionary<string, IFacetAccessible> MergeSimpleFacetContainers(IEnumerable<IDictionary<string, IFacetAccessible>> subMaps, BrowseRequest req)
             {
-                FacetSpec fs = req.GetFacetSpec(facet);
-
-                FacetSpec.FacetSortSpec sortSpec = fs.OrderBy;
-
-                IComparer<BrowseFacet> comparator;
-                if (FacetSpec.FacetSortSpec.OrderValueAsc.Equals(sortSpec))
+                Dictionary<string, Dictionary<object, BrowseFacet>> counts = new Dictionary<string, Dictionary<object, BrowseFacet>>();
+                foreach (Dictionary<string, IFacetAccessible> subMap in subMaps)
                 {
-                    comparator = FACET_VAL_COMPARATOR;
-                }
-                else if (FacetSpec.FacetSortSpec.OrderHitsDesc.Equals(sortSpec))
-                {
-                    comparator = FacetHitcountComparatorFactory.FACET_HITS_COMPARATOR;
-                }
-                else
-                {
-                    comparator = fs.CustomComparatorFactory.NewComparator();
-                }
-
-                Dictionary<object, BrowseFacet> facetValueCounts = counts[facet];
-                BrowseFacet[] facetArray = facetValueCounts.Values.ToArray();
-                System.Array.Sort(facetArray, comparator);
-
-                int numToShow = facetArray.Length;
-                if (req != null)
-                {
-                    FacetSpec fspec = req.GetFacetSpec(facet);
-                    if (fspec != null)
+                    foreach (KeyValuePair<string, IFacetAccessible> entry in subMap)
                     {
-                        int maxCount = fspec.MaxCount;
-                        if (maxCount > 0)
+                        Dictionary<object, BrowseFacet> count = counts[entry.Key];
+                        if (count == null)
                         {
-                            numToShow = Math.Min(maxCount, numToShow);
+                            count = new Dictionary<object, BrowseFacet>();
+                            counts.Add(entry.Key, count);
+                        }
+                        foreach (BrowseFacet facet in entry.Value.GetFacets())
+                        {
+                            object val = facet.Value;
+                            BrowseFacet oldValue = count[val];
+                            if (oldValue == null)
+                            {
+                                count.Add(val, new BrowseFacet(val, facet.HitCount));
+                            }
+                            else
+                            {
+                                oldValue.HitCount = oldValue.HitCount + facet.HitCount;
+                            }
                         }
                     }
                 }
 
-                BrowseFacet[] facets;
-                if (numToShow == facetArray.Length)
-                {
-                    facets = facetArray;
-                }
-                else
-                {
-                    facets = new BrowseFacet[numToShow];
-                    System.Array.Copy(facetArray, 0, facets, 0, numToShow);
-                }
+                Dictionary<string, IFacetAccessible> mergedFacetMap = new Dictionary<string, IFacetAccessible>();
 
-                MappedFacetAccessible mergedFacetAccessible = new MappedFacetAccessible(facets);
-                mergedFacetMap.Add(facet, mergedFacetAccessible);
+                foreach (string facet in counts.Keys)
+                {
+                    FacetSpec fs = req.GetFacetSpec(facet);
+
+                    FacetSpec.FacetSortSpec sortSpec = fs.OrderBy;
+
+                    IComparer<BrowseFacet> comparator;
+                    if (FacetSpec.FacetSortSpec.OrderValueAsc.Equals(sortSpec))
+                    {
+                        comparator = FACET_VAL_COMPARATOR;
+                    }
+                    else if (FacetSpec.FacetSortSpec.OrderHitsDesc.Equals(sortSpec))
+                    {
+                        comparator = FacetHitcountComparatorFactory.FACET_HITS_COMPARATOR;
+                    }
+                    else
+                    {
+                        comparator = fs.CustomComparatorFactory.NewComparator();
+                    }
+
+                    Dictionary<object, BrowseFacet> facetValueCounts = counts[facet];
+                    BrowseFacet[] facetArray = facetValueCounts.Values.ToArray();
+                    Array.Sort(facetArray, comparator);
+
+                    int numToShow = facetArray.Length;
+                    if (req != null)
+                    {
+                        FacetSpec fspec = req.GetFacetSpec(facet);
+                        if (fspec != null)
+                        {
+                            int maxCount = fspec.MaxCount;
+                            if (maxCount > 0)
+                            {
+                                numToShow = Math.Min(maxCount, numToShow);
+                            }
+                        }
+                    }
+
+                    BrowseFacet[] facets;
+                    if (numToShow == facetArray.Length)
+                    {
+                        facets = facetArray;
+                    }
+                    else
+                    {
+                        facets = new BrowseFacet[numToShow];
+                        Array.Copy(facetArray, 0, facets, 0, numToShow);
+                    }
+
+                    MappedFacetAccessible mergedFacetAccessible = new MappedFacetAccessible(facets);
+                    mergedFacetMap.Add(facet, mergedFacetAccessible);
+                }
+                return mergedFacetMap;
             }
-            return mergedFacetMap;
         }
     }
 }
