5a92bd6e4e5b72b7e0fde8f4a3570d4c53742b0f
Shad Storhaug <shad@shadstorhaug.com>
2014-12-16 05:08:11 +0700

    Added the BucketFacetCountCollector and BucketFacetHandler types (3.1.0).

---
+++ src/BoboBrowse.Net/Facets/Impl/BucketFacetCountCollector.cs
@@ -0,0 +1,156 @@
+﻿// Version compatibility level: 3.1.0
+namespace BoboBrowse.Net.Facets.Impl
+{
+    using BoboBrowse.Net.Facets.Data;
+    using BoboBrowse.Net.Util;
+    using Lucene.Net.Util;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BucketFacetCountCollector : IFacetCountCollector
+    {
+        private readonly String _name;
+        private readonly DefaultFacetCountCollector _subCollector;
+        private readonly FacetSpec _ospec;
+        private readonly IDictionary<string, string[]> _predefinedBuckets;
+        private int[] _collapsedCounts;
+        private TermStringList _bucketValues;
+        private readonly int _numdocs;
+
+        public BucketFacetCountCollector(string name, DefaultFacetCountCollector subCollector, FacetSpec ospec, IDictionary<string, string[]> predefinedBuckets, int numdocs)
+        {
+            _name = name;
+            _subCollector = subCollector;
+            _ospec = ospec;
+            _numdocs = numdocs;
+
+            _predefinedBuckets = predefinedBuckets;
+            _collapsedCounts = null;
+
+            _bucketValues = new TermStringList();
+            _bucketValues.Add("");
+
+            List<string> bucketArray = _predefinedBuckets.Keys.ToList();
+            bucketArray.Sort();
+            foreach (string bucket in bucketArray)
+            {
+                _bucketValues.Add(bucket);
+            }
+            _bucketValues.Seal();
+        }
+
+        private int[] GetCollapsedCounts()
+        {
+            if (_collapsedCounts == null)
+            {
+                _collapsedCounts = new int[_bucketValues.Count];
+                IFacetDataCache dataCache = _subCollector._dataCache;
+                ITermValueList subList = dataCache.ValArray;
+                int[] subcounts = _subCollector._count;
+                BitVector indexSet = new BitVector(subcounts.Length);
+                int c = 0;
+                int i = 0;
+                foreach (string val in _bucketValues)
+                {
+                    if (val.Length > 0)
+                    {
+                        string[] subVals = _predefinedBuckets.Get(val);
+                        int count = 0;
+                        foreach (string subVal in subVals)
+                        {
+                            int index = subList.IndexOf(subVal);
+                            if (index > 0)
+                            {
+                                int subcount = subcounts[index];
+                                count += subcount;
+                                if (!indexSet.Get(index))
+                                {
+                                    indexSet.Set(index);
+                                    c += dataCache.Freqs[index];
+                                }
+                            }
+                        }
+                        _collapsedCounts[i] = count;
+                    }
+                    i++;
+                }
+                _collapsedCounts[0] = (_numdocs - c);
+            }
+            return _collapsedCounts;
+        }
+
+        /// <summary>
+        /// get the total count of all possible elements 
+        /// </summary>
+        /// <returns></returns>
+        public virtual int[] GetCountDistribution()
+        {
+            return GetCollapsedCounts();
+        }
+
+        public virtual string Name
+        {
+            get { return _name; }
+        }
+
+        /// <summary>
+        /// get the facet of one particular bucket
+        /// </summary>
+        /// <param name="bucketValue"></param>
+        /// <returns></returns>
+        public BrowseFacet GetFacet(string bucketValue)
+        {
+            int index = _bucketValues.IndexOf(bucketValue);
+            if (index < 0)
+            {
+                return new BrowseFacet(bucketValue, 0);
+            }
+
+            int[] counts = GetCollapsedCounts();
+
+            return new BrowseFacet(bucketValue, counts[index]);
+        }
+
+        public int GetFacetHitsCount(object value)
+        {
+            int index = _bucketValues.IndexOf(value);
+            if (index < 0)
+            {
+                return 0;
+            }
+
+            int[] counts = GetCollapsedCounts();
+
+            return counts[index];
+        }
+
+        public sealed void Collect(int docid)
+        {
+            _subCollector.Collect(docid);
+        }
+
+        public sealed void CollectAll()
+        {
+            _subCollector.CollectAll();
+        }
+
+        public virtual IEnumerable<BrowseFacet> GetFacets()
+        {
+            int[] counts = GetCollapsedCounts();
+            return DefaultFacetCountCollector.GetFacets(_ospec, counts, counts.Length, _bucketValues);
+        }
+
+        public void Close()
+        {
+            _subCollector.Close();
+        }
+
+        public FacetIterator Iterator()
+        {
+            int[] counts = GetCollapsedCounts();
+            return new DefaultFacetIterator(_bucketValues, counts, counts.Length, true);
+        }
+    }
+}
---
+++ src/BoboBrowse.Net/Facets/Impl/BucketFacetHandler.cs
@@ -0,0 +1,166 @@
+﻿// Version compatibility level: 3.1.0
+namespace BoboBrowse.Net.Facets.Impl
+{
+    using BoboBrowse.Net.Facets.Data;
+    using BoboBrowse.Net.Facets.Filter;
+    using BoboBrowse.Net.Sort;
+    using BoboBrowse.Net.Util;
+    using Common.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BucketFacetHandler : FacetHandler<FacetDataNone>
+    {
+        private static ILog logger = LogManager.GetLogger<BucketFacetHandler>();
+        private readonly IDictionary<string, string[]> _predefinedBuckets;
+        private readonly string _name;
+        private readonly string _dependsOnFacetName;
+
+        public BucketFacetHandler(string name, IDictionary<string, string[]> predefinedBuckets, string dependsOnFacetName)
+            : base(name, new string[] { dependsOnFacetName })
+        {
+            _name = name;
+            _predefinedBuckets = predefinedBuckets;
+            _dependsOnFacetName = dependsOnFacetName;
+        }
+
+        public override DocComparatorSource GetDocComparatorSource()
+        {
+            var dependOnFacetHandler = GetDependedFacetHandler(_dependsOnFacetName);
+            return dependOnFacetHandler.GetDocComparatorSource();
+        }
+
+        public override string[] GetFieldValues(BoboIndexReader reader, int id)
+        {
+            var dependOnFacetHandler = GetDependedFacetHandler(_dependsOnFacetName);
+	        return dependOnFacetHandler.GetFieldValues(reader, id);
+        }
+
+        public override object[] GetRawFieldValues(BoboIndexReader reader, int id)
+        {
+            var dependOnFacetHandler = GetDependedFacetHandler(_dependsOnFacetName);
+            return dependOnFacetHandler.GetRawFieldValues(reader, id);
+        }
+
+        public override RandomAccessFilter BuildRandomAccessFilter(string bucketString, Properties prop)
+        {
+            var dependOnFacetHandler = GetDependedFacetHandler(_dependsOnFacetName);
+
+            string[] elems = _predefinedBuckets.Get(bucketString);
+
+            if (elems == null || elems.Length == 0) return EmptyFilter.GetInstance();
+            if (elems.Length == 1) return dependOnFacetHandler.BuildRandomAccessFilter(elems[0], prop);
+            return dependOnFacetHandler.BuildRandomAccessOrFilter(elems, prop, false);
+        }
+
+        public override RandomAccessFilter BuildRandomAccessAndFilter(string[] bucketStrings, Properties prop)
+        {
+            List<RandomAccessFilter> filterList = new List<RandomAccessFilter>();
+            var dependOnFacetHandler = GetDependedFacetHandler(_dependsOnFacetName);
+
+            foreach (string bucketString in bucketStrings)
+            {
+                string[] vals = _predefinedBuckets.Get(bucketString);
+                RandomAccessFilter filter = dependOnFacetHandler.BuildRandomAccessOrFilter(vals, prop, false);
+                if (filter == EmptyFilter.GetInstance()) return EmptyFilter.GetInstance();
+                filterList.Add(filter);
+            }
+            if (filterList.Count == 0) return EmptyFilter.GetInstance();
+            if (filterList.Count == 1) return filterList[0];
+            return new RandomAccessAndFilter(filterList);
+        }
+
+        public override RandomAccessFilter BuildRandomAccessOrFilter(string[] bucketStrings, Properties prop, bool isNot)
+        {
+            if (isNot)
+            {
+                RandomAccessFilter excludeFilter = BuildRandomAccessAndFilter(bucketStrings, prop);
+                return new RandomAccessNotFilter(excludeFilter);
+            }
+            else
+            {
+                var dependOnFacetHandler = GetDependedFacetHandler(_dependsOnFacetName);
+
+                HashSet<string> selections = new HashSet<string>();
+                foreach (string bucket in bucketStrings)
+                {
+                    string[] vals = _predefinedBuckets.Get(bucket);
+                    if (vals != null)
+                    {
+                        foreach (string val in vals)
+                        {
+                            selections.Add(val);
+                        }
+                    }
+                }
+                if (selections != null && selections.Count > 0)
+                {
+                    String[] sels = selections.ToArray();
+                    if (selections.Count == 1)
+                    {
+                        return dependOnFacetHandler.BuildRandomAccessFilter(sels[0], prop);
+                    }
+                    else
+                    {
+                        return dependOnFacetHandler.BuildRandomAccessOrFilter(sels, prop, false);
+                    }
+                }
+                else
+                {
+                    return EmptyFilter.GetInstance();
+                }
+            }
+        }
+
+        public override int GetNumItems(BoboIndexReader reader, int id)
+        {
+            var dependOnFacetHandler = GetDependedFacetHandler(_dependsOnFacetName);
+            IFacetDataCache data = dependOnFacetHandler.GetFacetData(reader);
+            return data.GetNumItems(id);
+        }
+
+        public override FacetCountCollectorSource GetFacetCountCollectorSource(BrowseSelection sel, FacetSpec fspec)
+        {
+            var dependOnFacetHandler = GetDependedFacetHandler(_dependsOnFacetName);
+            return new BucketFacetCountCollectorSource(_name, sel, fspec, _predefinedBuckets, dependOnFacetHandler);
+        }
+
+        public class BucketFacetCountCollectorSource : FacetCountCollectorSource
+        {
+            private readonly string _name;
+            private readonly BrowseSelection _sel;
+            private readonly FacetSpec _ospec;
+            private readonly IDictionary<string, string[]> _predefinedBuckets;
+            private readonly IFacetHandler _dependOnFacetHandler;
+
+            public BucketFacetCountCollectorSource(string name, BrowseSelection sel, FacetSpec ospec, IDictionary<string, string[]> predefinedBuckets, IFacetHandler dependOnFacetHandler)
+            {
+                _name = name;
+                _sel = sel;
+                _ospec = ospec;
+                _predefinedBuckets = predefinedBuckets;
+                _dependOnFacetHandler = dependOnFacetHandler;
+            }
+
+            public override IFacetCountCollector GetFacetCountCollector(BoboIndexReader reader, int docBase)
+            {
+                IFacetCountCollector defaultCollector = _dependOnFacetHandler.GetFacetCountCollectorSource(_sel, _ospec).GetFacetCountCollector(reader, docBase);
+                if (defaultCollector is DefaultFacetCountCollector)
+                {
+                    return new BucketFacetCountCollector(_name, (DefaultFacetCountCollector)defaultCollector, _ospec, _predefinedBuckets, reader.NumDocs());
+                }
+                else
+                {
+                    throw new InvalidOperationException("dependent facet handler must build DefaultFacetCountCollector");
+                }
+            }
+        }
+
+        public override FacetDataNone Load(BoboIndexReader reader)
+        {
+            return FacetDataNone.instance;
+        }
+    }
+}
