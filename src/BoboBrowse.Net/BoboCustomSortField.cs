50f664aefbedec3ebada864213f54ec84d1e710e
Shad Storhaug <shad@shadstorhaug.com>
2014-12-16 03:23:25 +0700

    Implemented BoboCustomSortField (version 3.1.0).

---
+++ src/BoboBrowse.Net/BoboCustomSortField.cs
@@ -0,0 +1,25 @@
+﻿// Version compatibility level: 3.1.0
+namespace BoboBrowse.Net
+{
+    using BoboBrowse.Net.Sort;
+    using Lucene.Net.Search;
+    using System;
+
+    public class BoboCustomSortField : SortField
+    {
+        private static long serialVersionUID = 1L;
+
+	    private readonly DocComparatorSource _factory;
+
+        public BoboCustomSortField(string field, bool reverse, DocComparatorSource factory)
+            : base(field, SortField.CUSTOM, reverse)
+        {
+            _factory = factory;
+        }
+
+        public virtual DocComparatorSource GetCustomComparatorSource()
+        {
+            return _factory;
+        }
+    }
+}
