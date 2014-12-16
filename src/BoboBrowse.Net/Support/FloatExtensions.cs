85acd1f6afc9bec8aedfe01fc74d95050a4fc7c2
Shad Storhaug <shad@shadstorhaug.com>
2014-12-16 17:10:27 +0700

    Added Support namespace for the .NET specific conversions. Created IEnumerableExtensions and FloatExtensions types.

---
+++ src/BoboBrowse.Net/Support/FloatExtensions.cs
@@ -0,0 +1,30 @@
+﻿// Version compatibility level: 3.1.0
+namespace BoboBrowse.Net.Support
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Based on answer from: http://stackoverflow.com/questions/7173677/c-sharp-how-to-convert-float-to-int
+    /// </summary>
+    public static class FloatExtensions
+    {
+        public static int FloatToIntBits(this float f)
+        {
+            var s = new MemoryStream();
+            var w = new BinaryWriter(s);
+            w.Write(f);
+            s.Position = 0;
+            var r = new BinaryReader(s);
+            return r.ReadInt32();
+        }
+    }
+
+    public static class Float
+    {
+        public static int floatToIntBits(float f)
+        {
+            return f.FloatToIntBits();
+        }
+    }
+}
---
+++ src/BoboBrowse.Net/Support/IEnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Version compatibility level: 3.1.0
+namespace BoboBrowse.Net.Support
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    // TODO: need to search for every place this was used in 3.1.0 and put it back in
+    public static class IEnumerableExtensions
+    {
+        public static T Get<T>(this IEnumerable<T> enumerable, int index)
+        {
+            if (index < enumerable.Count())
+                return enumerable.ElementAt(index);
+            else
+                return default(T);
+        }
+    }
+}
