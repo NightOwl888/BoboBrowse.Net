0ec5de9f2ce9908930e52952a80597d72fc3619f
Shad Storhaug <shad@shadstorhaug.com>
2014-12-21 04:44:58 +0700

    Ported TermLongListTest.

---
+++ src/BoboBrowse.Tests/Facets/Data/TermLongListTest.cs
@@ -0,0 +1,87 @@
+﻿// Version compatibility level: 3.1.0
+namespace BoboBrowse.Net.Facets.Data
+{
+    using BoboBrowse.Net.Support;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    [TestFixture]
+    public class TermLongListTest
+    {
+        [Test]
+        public void Test1TwoNegativeValues()
+        {
+            TermLongList list = new TermLongList();
+            list.Add(null);
+            list.Add("-1");
+            list.Add("-2");
+            list.Add("0");
+            list.Add("1");
+
+            list.Seal();
+            Assert.True(Arrays.Equals(new long[] { 0, -2, -1, 0, 1 }, list.ToArray()));
+        }
+
+        [Test]
+        public void Test2ThreeNegativeValues()
+        {
+            TermLongList list = new TermLongList();
+            list.Add(null);
+            list.Add("-1");
+            list.Add("-2");
+            list.Add("-3");
+            list.Add("0");
+            list.Add("1");
+
+            list.Seal();
+            Assert.True(Arrays.Equals(new long[] { 0, -3, -2, -1, 0, 1 }, list.ToArray()));
+        }
+
+        [Test]
+        public void Test2aThreeNegativeValuesInt()
+        {
+            TermIntList list = new TermIntList();
+            list.Add(null);
+            list.Add("-1");
+            list.Add("-2");
+            list.Add("-3");
+            list.Add("0");
+            list.Add("1");
+
+            list.Seal();
+            Assert.True(Arrays.Equals(new int[] { 0, -3, -2, -1, 0, 1 }, list.ToArray()));
+        }
+
+        [Test]
+        public void Test2bThreeNegativeValuesShort()
+        {
+            TermShortList list = new TermShortList();
+            list.Add(null);
+            list.Add("-1");
+            list.Add("-2");
+            list.Add("-3");
+            list.Add("0");
+            list.Add("1");
+
+            list.Seal();
+            Assert.True(Arrays.Equals(new short[] { 0, -3, -2, -1, 0, 1 }, list.ToArray()));
+        }
+
+        public void Test3ThreeNegativeValuesWithoutDummy()
+        {
+            TermLongList list = new TermLongList();
+
+            list.Add("-1");
+            list.Add("-2");
+            list.Add("-3");
+            list.Add("0");
+            list.Add("1");
+
+            list.Seal();
+            Assert.True(Arrays.Equals(new long[] { -3, -2, -1, 0, 1 }, list.ToArray()));
+        }
+    }
+}
