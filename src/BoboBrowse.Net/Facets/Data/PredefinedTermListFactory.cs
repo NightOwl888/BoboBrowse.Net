759241ccc8027bb980bd38119c49b383764873df
Shad Storhaug <shad@shadstorhaug.com>
2014-12-23 11:10:17 +0700

    Created a non-generic PredefinedTermListFactory so the type of list can be determined at runtime, not just at compile time.

--- src/BoboBrowse.Net/Facets/Data/PredefinedTermListFactory.cs
+++ src/BoboBrowse.Net/Facets/Data/PredefinedTermListFactory.cs
@@ -22,8 +22,40 @@ namespace BoboBrowse.Net.Facets.Data
     /// </list>
     /// </summary>
     /// <typeparam name="TSupported">The type of term list to build. Supported types are int, float, char, double, long, and DateTime.</typeparam>
-    public class PredefinedTermListFactory<TSupported> : TermListFactory
+    public class PredefinedTermListFactory<TSupported> : PredefinedTermListFactory
     {
+        public PredefinedTermListFactory(string formatString, IFormatProvider formatProvider)
+            : base(typeof(TSupported), formatString, formatProvider)
+        { }
+
+        public PredefinedTermListFactory(string formatString)
+            : this(formatString, null)
+        { }
+
+        public PredefinedTermListFactory()
+            : this(null, null)
+        { }
+    }
+
+    /// <summary>
+    /// Similar to the original Java version of the PredefinedTermListFactory, but has been optimized to use
+    /// .NET generics and .NET string formatting.
+    /// 
+    /// Types supported:
+    /// <list type="bullet">
+    ///     <item>int</item>
+    ///     <item>float</item>
+    ///     <item>char</item>
+    ///     <item>double</item>
+    ///     <item>short</item>
+    ///     <item>long</item>
+    ///     <item>DateTime</item>
+    /// </list>
+    /// </summary>
+    /// <typeparam name="TSupported">The type of term list to build. Supported types are int, float, char, double, long, and DateTime.</typeparam>
+    public class PredefinedTermListFactory: TermListFactory
+    {
+        private readonly Type listType;
         private readonly string formatString;
         private readonly IFormatProvider formatProvider;
         protected IDictionary<Type, Type> supportedTypes = new Dictionary<Type, Type>()
@@ -37,27 +69,27 @@ namespace BoboBrowse.Net.Facets.Data
             { typeof(DateTime), typeof(TermDateList) }
         };
 
-        public PredefinedTermListFactory(string formatString, IFormatProvider formatProvider)
+        public PredefinedTermListFactory(Type type, string formatString, IFormatProvider formatProvider)
         {
-            if (!this.supportedTypes.ContainsKey(typeof(TSupported)))
+            if (!supportedTypes.ContainsKey(type))
                 throw new ArgumentException(string.Format("Type '{0}' is not supported. The only supported types are:{2}{1}",
-                    typeof(TSupported).FullName, string.Join(Environment.NewLine, this.supportedTypes.Keys.Select(key => key.FullName).ToArray()), Environment.NewLine));
+                    type.FullName, string.Join(Environment.NewLine, supportedTypes.Keys.Select(key => key.FullName).ToArray()), Environment.NewLine));
 
+            this.listType = supportedTypes[type];
             this.formatString = formatString;
             this.formatProvider = formatProvider;
         }
 
-        public PredefinedTermListFactory(string formatString)
-            : this(formatString, null)
+        public PredefinedTermListFactory(Type listType, string formatString)
+            : this(listType, formatString, null)
         { }
 
-        public PredefinedTermListFactory()
-            : this(null, null)
+        public PredefinedTermListFactory(Type listType)
+            : this(listType, null, null)
         { }
 
         public override ITermValueList CreateTermList(int capacity)
         {
-            var listType = this.supportedTypes[typeof(TSupported)];
             // we treat char type separate as it does not have a format string
             if (typeof(TermCharList).Equals(listType))
             {
