f11c1139002317cbee389d640a625bcfb9df5456
Shad Storhaug <shad@shadstorhaug.com>
2014-12-25 16:09:07 +0700

    Fixed configuration of BoboBrowse.Net.Console so it will compile correctly.

--- src/BoboBrowse.Net.Console/Program.cs
+++ src/BoboBrowse.Net.Console/Program.cs
@@ -1,14 +1,12 @@
 ﻿namespace BoboBrowse.Net.Console
 {
-    // TODO: Find out why the namespace is failing to resolve here.
-    //using BoboBrowse.Net.Client;
+    using BoboBrowse.Net.Client;
 
     class Program
     {
         static int Main(string[] args)
         {
-            return 0;
-            //return BoboCmdlineApp.Main(args);
+            return BoboCmdlineApp.Main(args);
         }
     }
 }
