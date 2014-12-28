4d6bce84f9712b535cf9cd9ad5a253263bda215b
Shad Storhaug <shad@shadstorhaug.com>
2014-12-28 11:46:45 +0700

    Added bobo.spring file to the LuceneIndex folder and added documentation about the 2 facet handler configuration options.

--- src/CarDemo/CarDemo/BoboServices/BrowseService.cs
+++ src/CarDemo/CarDemo/BoboServices/BrowseService.cs
@@ -16,21 +16,25 @@ namespace CarDemo.BoboServices
         {
             string indexDir = HostingEnvironment.MapPath(ConfigurationManager.AppSettings["LuceneIndexDirectory"]);
 
-            // TODO: configure facet handlers via XML
-            var facetHandlers = new List<IFacetHandler>();
-            facetHandlers.Add(new SimpleFacetHandler("color") { TermCountSize = BoboBrowse.Net.Facets.TermCountSize.Small });
-            facetHandlers.Add(new SimpleFacetHandler("category") { TermCountSize = BoboBrowse.Net.Facets.TermCountSize.Medium });
-            facetHandlers.Add(new PathFacetHandler("city") { Separator = "/" });
-            facetHandlers.Add(new PathFacetHandler("makemodel") { Separator = "/" });
-            facetHandlers.Add(new RangeFacetHandler("year", new PredefinedTermListFactory<int>("00000000000000000000"), new string[] { "[1993 TO 1994]", "[1995 TO 1996]", "[1997 TO 1998]", "[1999 TO 2000]", "[2001 TO 2002]" }));
-            facetHandlers.Add(new RangeFacetHandler("price", new PredefinedTermListFactory<float>("00000000000000000000"), new string[] { "[2001 TO 6700]", "[6800 TO 9900]", "[10000 TO 13100]", "[13200 TO 17300]", "[17400 TO 19500]" }));
-            facetHandlers.Add(new RangeFacetHandler("mileage", new PredefinedTermListFactory<int>("00000000000000000000"), new string[] { "[* TO 12500]", "[12501 TO 15000]", "[15001 TO 17500]", "[17501 TO *]" }));
-            facetHandlers.Add(new MultiValueFacetHandler("tags"));
+            // This is the equivalent code to what is specified in the "/LuceneIndex/bobo.spring" file. You could just as well
+            // specify this configuration here and pass it to another overload of BoboIndexReader.GetInstance that accepts 
+            // an IEnumerable<IFacetHandler> as an argument.
+
+            //var facetHandlers = new List<IFacetHandler>();
+            //facetHandlers.Add(new SimpleFacetHandler("color") { TermCountSize = BoboBrowse.Net.Facets.TermCountSize.Small });
+            //facetHandlers.Add(new SimpleFacetHandler("category") { TermCountSize = BoboBrowse.Net.Facets.TermCountSize.Medium });
+            //facetHandlers.Add(new PathFacetHandler("city") { Separator = "/" });
+            //facetHandlers.Add(new PathFacetHandler("makemodel") { Separator = "/" });
+            //facetHandlers.Add(new RangeFacetHandler("year", new PredefinedTermListFactory<int>("00000000000000000000"), new string[] { "[1993 TO 1994]", "[1995 TO 1996]", "[1997 TO 1998]", "[1999 TO 2000]", "[2001 TO 2002]" }));
+            //facetHandlers.Add(new RangeFacetHandler("price", new PredefinedTermListFactory<float>("00000000000000000000"), new string[] { "[2001 TO 6700]", "[6800 TO 9900]", "[10000 TO 13100]", "[13200 TO 17300]", "[17400 TO 19500]" }));
+            //facetHandlers.Add(new RangeFacetHandler("mileage", new PredefinedTermListFactory<int>("00000000000000000000"), new string[] { "[* TO 12500]", "[12501 TO 15000]", "[15001 TO 17500]", "[17501 TO *]" }));
+            //facetHandlers.Add(new MultiValueFacetHandler("tags"));
+
 
             System.IO.DirectoryInfo idxDir = new System.IO.DirectoryInfo(indexDir);
             using (IndexReader reader = IndexReader.Open(FSDirectory.Open(idxDir), true))
             {
-                using (BoboIndexReader boboReader = BoboIndexReader.GetInstance(reader, facetHandlers))
+                using (BoboIndexReader boboReader = BoboIndexReader.GetInstance(reader))
                 {
                     using (BoboBrowser browser = new BoboBrowser(boboReader))
                     {
