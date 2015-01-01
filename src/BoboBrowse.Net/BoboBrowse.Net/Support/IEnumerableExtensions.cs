b2c40c65d28284047f9cd07879ab07508c77643a
Shad Storhaug <shad@shadstorhaug.com>
2015-01-01 23:37:19 +0700

    Put IListExtensions into its own file.

--- src/BoboBrowse.Net/BoboBrowse.Net/Support/IEnumerableExtensions.cs
+++ src/BoboBrowse.Net/BoboBrowse.Net/Support/IEnumerableExtensions.cs
@@ -1,7 +1,6 @@
 ﻿// Version compatibility level: 3.1.0
 namespace BoboBrowse.Net.Support
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -21,40 +20,6 @@ namespace BoboBrowse.Net.Support
                 return enumerable.ElementAt(index);
             else
                 return defaultValue;
-        }
-    }
-
-    public static class IListExtensions
-    {
-        public static T Poll<T>(this IList<T> list) where T: class
-        {
-            T value = list.Get(0);
-            if (list.Count > 0)
-                list.RemoveAt(0);
-            return value;
-        }
-
-        public static T Poll<T>(this IList<T> list, T defaultValue)
-        {
-            T value = list.Get(0, defaultValue);
-            if (list.Count > 0)
-                list.RemoveAt(0);
-            return value;
-        }
-
-        /// <summary>
-        /// Removes elements of this type-specific list
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <param name="from">the start index (inclusive).</param>
-        /// <param name="to">the end index (exclusive).</param>
-        public static void RemoveElements<T>(this IList<T> list, int from, int to)
-        {
-            for (int i = (to - 1); i >= from; i--)
-            {
-                list.RemoveAt(i);
-            }
         }
     }
 }
---
+++ src/BoboBrowse.Net/BoboBrowse.Net/Support/IListExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace BoboBrowse.Net.Support
+{
+    using System.Collections.Generic;
+
+    public static class IListExtensions
+    {
+        public static T Poll<T>(this IList<T> list) where T : class
+        {
+            T value = list.Get(0);
+            if (list.Count > 0)
+                list.RemoveAt(0);
+            return value;
+        }
+
+        public static T Poll<T>(this IList<T> list, T defaultValue)
+        {
+            T value = list.Get(0, defaultValue);
+            if (list.Count > 0)
+                list.RemoveAt(0);
+            return value;
+        }
+
+        /// <summary>
+        /// Removes elements of this type-specific list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="from">the start index (inclusive).</param>
+        /// <param name="to">the end index (exclusive).</param>
+        public static void RemoveElements<T>(this IList<T> list, int from, int to)
+        {
+            for (int i = (to - 1); i >= from; i--)
+            {
+                list.RemoveAt(i);
+            }
+        }
+    }
+}
