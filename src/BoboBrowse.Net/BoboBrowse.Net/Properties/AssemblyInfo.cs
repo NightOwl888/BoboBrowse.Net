5ee4f5ab037eaa514e667e959bef030e1902946d
Shad Storhaug <shad@shadstorhaug.com>
2015-04-14 03:56:52 +0700

    Removed untidy space after the URL in the description in nuspec file and AssemblyInfo file.

--- src/BoboBrowse.Net/BoboBrowse.Net/Properties/AssemblyInfo.cs
+++ src/BoboBrowse.Net/BoboBrowse.Net/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BoboBrowse.Net")]
-[assembly: AssemblyDescription("BoboBrowse.Net is a faceted browse engine implemented on a top of Lucene.Net. Originally ported from wonderful Java library developed by John Wang (http://javasoze.github.com/bobo/ ). While Lucene.Net is good with unstructured data, BoboBrowse.Net fills in the missing piece to handle semi-structured and structured data.")]
+[assembly: AssemblyDescription("BoboBrowse.Net is a faceted browse engine implemented on a top of Lucene.Net. Originally ported from wonderful Java library developed by John Wang (http://javasoze.github.com/bobo/). While Lucene.Net is good with unstructured data, BoboBrowse.Net fills in the missing piece to handle semi-structured and structured data.")]
 [assembly: AssemblyProduct("BoboBrowse.Net")]
 
 // Allow the tests to use internal members of this assembly.
