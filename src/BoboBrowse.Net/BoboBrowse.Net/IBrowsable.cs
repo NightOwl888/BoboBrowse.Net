72591e9248c1a63be444de99accf8a4b87bab1bb
Shad Storhaug <shad@shadstorhaug.com>
2016-07-17 17:29:26 +0700

    Updated IBrowsable.cs to 4.0.2 compatibility

--- src/BoboBrowse.Net/BoboBrowse.Net/IBrowsable.cs
+++ src/BoboBrowse.Net/BoboBrowse.Net/IBrowsable.cs
@@ -17,15 +17,17 @@
 //* See the License for the specific language governing permissions and
 //* limitations under the License.
 
-// Version compatibility level: 3.2.0
+// Version compatibility level: 4.0.2
 namespace BoboBrowse.Net
 {
     using BoboBrowse.Net.Facets;
     using BoboBrowse.Net.Sort;
+    using Lucene.Net.Index;
     using Lucene.Net.Search;
+    using Lucene.Net.Search.Similarities;
     using System.Collections.Generic;
 
-    public interface IBrowsable : Searchable
+    public interface IBrowsable
     {
         /// <summary>
         /// Generates a merged BrowseResult from the supplied <see cref="T:BrowseRequest"/>.
@@ -34,34 +36,8 @@ namespace BoboBrowse.Net
         /// <param name="req"><see cref="T:BrowseRequest"/> for generating the facets.</param>
         /// <param name="hitCollector">A <see cref="T:Lucene.Net.Search.Collector"/> for the hits generated during a search.</param>
         /// <param name="facets">A dictionary of all of the facet collections (output).</param>
-        void Browse(BrowseRequest req, 
-	        Collector hitCollector,
-	        IDictionary<string, IFacetAccessible> facets);
-
-        /// <summary>
-        /// Generates a merged BrowseResult from the supplied <see cref="T:BrowseRequest"/>.
-        /// The results are put into a Lucene.Net <see cref="T:Lucene.Net.Search.Collector"/> and a <see cref="T:System.Collections.Generic.IDictionary{System.String, IFacetAccessible}"/>.
-        /// </summary>
-        /// <param name="req"><see cref="T:BrowseRequest"/> for generating the facets.</param>
-        /// <param name="hitCollector">A <see cref="T:Lucene.Net.Search.Collector"/> for the hits generated during a search.</param>
-        /// <param name="facets">A dictionary of all of the facet collections (output).</param>
         /// <param name="start">The offset value for the document number.</param>
         void Browse(BrowseRequest req, 
-	        Collector hitCollector,
-	        IDictionary<string, IFacetAccessible> facets,
-	        int start);
-
-        /// <summary>
-        /// Generates a merged BrowseResult from the supplied <see cref="T:BrowseRequest"/> and a <see cref="T:Lucene.Net.Search.Weight"/>.
-        /// The results are put into a Lucene.Net <see cref="T:Lucene.Net.Search.Collector"/> and a <see cref="T:System.Collections.Generic.IDictionary{System.String, IFacetAccessible}"/>.
-        /// </summary>
-        /// <param name="req"><see cref="T:BrowseRequest"/> for generating the facets.</param>
-        /// <param name="weight">A <see cref="T:Lucene.Net.Search.Weight"/> instance to alter the score of the queries in a multiple index scenario.</param>
-        /// <param name="hitCollector">A <see cref="T:Lucene.Net.Search.Collector"/> for the hits generated during a search.</param>
-        /// <param name="facets">A dictionary of all of the facet collections (output).</param>
-        /// <param name="start">The offset value for the document number.</param>
-        void Browse(BrowseRequest req, 
-	        Weight weight,
 	        Collector hitCollector,
 	        IDictionary<string, IFacetAccessible> facets,
 	        int start);
@@ -92,7 +68,9 @@ namespace BoboBrowse.Net
         /// <returns>The facet handler instance.</returns>
         IFacetHandler GetFacetHandler(string name);
 
-        Similarity Similarity { get; set; }
+        IDictionary<string, IFacetHandler> FacetHandlerMap { get; }
+
+        Similarity Similarity { set; }
 
         /// <summary>
         /// Return the string representation of the values of a field for the given doc.
@@ -116,10 +94,12 @@ namespace BoboBrowse.Net
         /// <returns>The total number of documents.</returns>
         int NumDocs();
 
-        SortCollector GetSortCollector(SortField[] sort, Lucene.Net.Search.Query q, int offset, int count, bool fetchStoredFields, IEnumerable<string> termVectorsToFetch, bool forceScoring, string[] groupBy, int maxPerGroup, bool collectDocIdCache);
+        SortCollector GetSortCollector(SortField[] sort, Lucene.Net.Search.Query q, int offset, int count, 
+            bool fetchStoredFields, IEnumerable<string> termVectorsToFetch, string[] groupBy, int maxPerGroup, 
+            bool collectDocIdCache);
+
+        void DoClose(); // TODO: Should this be IDisposable?
 
         Explanation Explain(Lucene.Net.Search.Query q, int docid);
-
-        IDictionary<string, IFacetHandler> FacetHandlerMap { get; }
     }
 }
