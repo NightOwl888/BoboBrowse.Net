c3584acb98b373ea6a41cfbc30347773a17325ba
Shad Storhaug <shad@shadstorhaug.com>
2016-07-19 00:15:13 +0700

    Added stubs for Lucene.Net.QueryParser.Classic.QueryParser and Lucene.Net.Analysis.Standard.StandardAnalyzer since they have not yet been implmented in Lucene.Net, but they are required to make BoboBrowse.Net compile.

---
+++ src/BoboBrowse.Net/BoboBrowse.Net/Support/Stub/StandardAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿//* Bobo Browse Engine - High performance faceted/parametric search implementation 
+//* that handles various types of semi-structured data.  Originally written in Java.
+//*
+//* Ported and adapted for C# by Shad Storhaug.
+//*
+//* Copyright (C) 2005-2015  John Wang
+//*
+//* Licensed under the Apache License, Version 2.0 (the "License");
+//* you may not use this file except in compliance with the License.
+//* You may obtain a copy of the License at
+//*
+//*   http://www.apache.org/licenses/LICENSE-2.0
+//*
+//* Unless required by applicable law or agreed to in writing, software
+//* distributed under the License is distributed on an "AS IS" BASIS,
+//* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//* See the License for the specific language governing permissions and
+//* limitations under the License.
+
+namespace Lucene.Net.Analysis.Standard
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Lucene.Net.Analysis;
+    using Lucene.Net.Util;
+
+    /// <summary>
+    /// This class is a temporary stub so the project will compile until an implementation is available in Lucene.Net
+    /// </summary>
+    public class StandardAnalyzer : Analyzer
+    {
+        public StandardAnalyzer(LuceneVersion version)
+        {
+        }
+
+        public override Analyzer.TokenStreamComponents CreateComponents(string fieldName, TextReader reader)
+        {
+            return null;
+        }
+    }
+}
